6572816e37f606b731bf4fc733f34cc5a735307d
Amit Bhargava <amit.bhargava@absg.com>
2020-07-15 11:48:30 -0500

    Updates

---
+++ precache-manifest.f0a763f595d979fb2ba9af19863e8a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ffbd7f99b7c747afbf03ce5269b573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74f73530c022ae452906",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "871033d27df5e7ff0604",
+    "url": "/static/js/2.c5125b9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5125b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f73530c022ae452906",
+    "url": "/static/js/main.7237f536.chunk.js"
+  },
+  {
+    "revision": "ec41dc96a5e2f637b885",
+    "url": "/static/js/runtime-main.7b09a790.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
